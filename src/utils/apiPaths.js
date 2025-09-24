14c24383d95ef6815d71a498dd4731907461b52e
unknown <joshuashalim15@gmail.com>
2025-09-24 22:25:21 +0500

    updated filess .env

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,7 +1,7 @@
 // src/utils/apiPaths.js
 
 // Use environment variable for base URL (Vite style)
-export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1";
+export const BASE_URL = import.meta.env.VITE_API_URL || "https://expense-tracker-backend-production-04ed.up.railway.app/api/v1";
 
 export const API_PATHS = {
   AUTH: {
