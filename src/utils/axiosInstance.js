c86e2eee79ce2c3abda7f774828ea3bf2b8e2c42
unknown <joshuashalim15@gmail.com>
2025-09-24 21:51:31 +0500

    Updated CORS

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -15,19 +15,6 @@ const axiosInstance = axios.create({
 // Request Interceptor
 axiosInstance.interceptors.request.use(
   (config) => {
-    const accessToken = localStorage.getItem("accessToken");
-    if (accessToken) {
-      config.headers.Authorization = `Bearer ${accessToken}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-// Response Interceptor
-// ✅ Attach token automatically to every request
-axiosInstance.interceptors.request.use(
-  (config) => {
     const token = localStorage.getItem("token"); // 👈 use "token" consistently
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
